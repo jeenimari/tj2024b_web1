f84ce8ad616f9afe36dc52fae04225e0c7c0de7c
jeenimari <gha0189@gmail.com>
2025-01-27 13:17:58 +0900

    01/27 커밋

---
+++ tj2024b_wep1/src/main/java/web/model/dto/MemberDto.java
@@ -0,0 +1,22 @@
+package web.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor // 깡통생성자
+@AllArgsConstructor //모든 매개변수 생성자코드 자동
+@Getter
+@Setter
+@ToString
+public class MemberDto {
+        private int mno;                // 회원번호
+        private String mid;                // 아이디 
+        private String mpwd;        // 비밀번호
+        private String mname;        // 이름
+        private String mphone;        // 연락처 
+        private String mdate;        // 가입일 
+}
+
